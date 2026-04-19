30cb4b545fe0dc995f5f80242835d6220a958f67
Prachiiiiiiiii <prachiaswal15@gmail.com>
2026-04-19 22:53:16 +0530

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Student Scoreboard</h1>
+      <span>we dev assignment 3</span>
+    </header>
+  );
+}
+
+export default Header;
